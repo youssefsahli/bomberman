98e3f486bfc8bb4b4e94edaedc6af880e0c6b701
Youssef Sahli <sahli.youssef@ymail.com>
2024-04-11 21:38:27 +0200

    init gdx application

---
+++ desktop/src/dev/bomberman/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package dev.bomberman;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import dev.bomberman.BombermanApplication;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Bomberman");
+		new Lwjgl3Application(new BombermanApplication(), config);
+	}
+}
