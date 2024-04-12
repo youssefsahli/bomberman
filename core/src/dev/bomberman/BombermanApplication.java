bd3ccaeedb9fed20933bab82d69cb9da447e4663
Youssef Sahli <sahli.youssef@ymail.com>
2024-04-13 00:34:16 +0200

    add effect assets

--- core/src/dev/bomberman/BombermanApplication.java
+++ core/src/dev/bomberman/BombermanApplication.java
@@ -1,31 +1,41 @@
 package dev.bomberman;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class BombermanApplication extends ApplicationAdapter {
   SpriteBatch batch;
-  Texture img;
+  private FitViewport viewport;
+  private Camera camera;
+  private Texture player;
+  private Texture block;
+  private Texture bomb;
 
   @Override
   public void create() {
     batch = new SpriteBatch();
-    img = new Texture("badlogic.jpg");
+    camera = new PerspectiveCamera();
+    viewport = new FitViewport(800, 640, camera);
   }
 
   @Override
   public void render() {
-    ScreenUtils.clear(1, 0, 0, 1);
+    ScreenUtils.clear(0, 0, 0, 0);
     batch.begin();
-    batch.draw(img, 0, 0);
     batch.end();
   }
 
   @Override
   public void dispose() {
     batch.dispose();
-    img.dispose();
+  }
+  @Override
+  public void resize(int width,  int heigth) {
+	 viewport.update(width, heigth);
   }
 }
--- desktop/src/dev/bomberman/DesktopLauncher.java
+++ desktop/src/dev/bomberman/DesktopLauncher.java
@@ -2,7 +2,7 @@ package dev.bomberman;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import dev.bomberman.BombermanApplication;
+import com.badlogic.gdx.graphics.Color;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
@@ -10,6 +10,9 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Bomberman");
+		config.setWindowedMode(800, 640);
+		config.setInitialBackgroundColor(Color.BLACK);
+		
 		new Lwjgl3Application(new BombermanApplication(), config);
 	}
 }
