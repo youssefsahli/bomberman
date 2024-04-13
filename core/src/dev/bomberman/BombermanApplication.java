b6c364314a7374786ceedc3f52379d2314fc3d81
Youssef Sahli <sahli.youssef@ymail.com>
2024-04-13 13:34:01 +0200

    working setup

--- core/src/dev/bomberman/BombermanApplication.java
+++ core/src/dev/bomberman/BombermanApplication.java
@@ -1,24 +1,27 @@
 package dev.bomberman;
 
+import com.badlogic.ashley.core.Engine;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class BombermanApplication extends ApplicationAdapter {
-  SpriteBatch batch;
   private FitViewport viewport;
   private Camera camera;
   private Texture player;
   private Texture block;
   private Texture bomb;
+  private Engine ecs = new Engine();
+  private Animation<TextureRegion> idleAnimation;
 
   @Override
   public void create() {
-    batch = new SpriteBatch();
     camera = new PerspectiveCamera();
     viewport = new FitViewport(800, 640, camera);
   }
@@ -26,13 +29,12 @@ public class BombermanApplication extends ApplicationAdapter {
   @Override
   public void render() {
     ScreenUtils.clear(0, 0, 0, 0);
-    batch.begin();
-    batch.end();
   }
 
   @Override
   public void dispose() {
-    batch.dispose();
+    ecs.removeAllEntities();
+    ecs.removeAllSystems();
   }
   @Override
   public void resize(int width,  int heigth) {
---
+++ core/src/dev/bomberman/world/World.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package dev.bomberman.world;
+
+import com.badlogic.ashley.core.Engine;
+
+/**
+ * 
+ */
+public class World extends Engine {
+	
+}
